fe87f52d0fd212ade5fb3b54fbdf6bd6f78f7d1e
mybluewebsite <mybluewebsite@dontsp.am>
2026-03-10 10:35:35 +0000

    amend vite.congfig.js file

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  // 1. Remove Console Logs and Debuggers in Production
+  base: '/',
   esbuild: {
     drop: ['console', 'debugger'],
   },
-
-  // 2. Prevent Source Maps from being generated
   build: {
     sourcemap: false, 
   }
