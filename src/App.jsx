fe70ce2f7af491dfa3f2947291067ecf7179db0d
mybluewebsite <mybluewebsite@dontsp.am>
2026-03-10 10:20:33 +0000

    amending react code for live webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // ------------------------------------------------------------------
 // 1. LAYOUT WRAPPER (Keeps Navbar & Footer persistent across pages)
