aeda775242e10a705a32f7a5979961e36565c033
mybluewebsite <mybluewebsite@dontsp.am>
2026-03-10 09:15:15 +0000

    Updated vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // 1. Remove Console Logs and Debuggers in Production
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
+
+  // 2. Prevent Source Maps from being generated
+  build: {
+    sourcemap: false, 
+  }
 })
